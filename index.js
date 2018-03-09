db01a7860e2f677727631dae3776f08594594e55
Butchy1596 <dterra0726@gmail.com>
2018-03-09 00:33:54 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,6 +3,6 @@ const app = "I don't do much."
 var kittens =["Milo", "Otis", "Garfield"]
 var name = "Ralph"
 function destructivelyAppendKitten(name){
-  name.pop(kittens)
+  kittens.pop(name)
   return kittens
 }
