53fb9abcb5805f569c895ab1dda221d323481065
Butchy1596 <dterra0726@gmail.com>
2018-03-09 01:40:47 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -38,5 +38,5 @@ function prependKitten(name){
 
 //Arrays removeLastKitten() removes the last kitten in the kittens array and returns a new array, leaving the kittens array unchanged:
 function removeLastKitten(){
-  return [...kittens.pop(-1)]
+  return kittens.slice(0, kittens.length - 1);
 }
