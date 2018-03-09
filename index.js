e65969216b9e0d7f65a7daf0f03935afb96d0b4d
Butchy1596 <dterra0726@gmail.com>
2018-03-09 01:13:29 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,26 +3,26 @@ const app = "I don't do much."
 var kittens =["Milo", "Otis", "Garfield"]
 var name = "Ralph"
 
-//1) Arrays destructivelyAppendKitten(name) appends a kitten to the end of the kittens array:
+//Arrays destructivelyAppendKitten(name) appends a kitten to the end of the kittens array:
 function destructivelyAppendKitten(name){
   kittens.push(name);
   return kittens
 }
 
-//2) Arrays destructivelyPrependKitten(name) prepends a kitten to the beginning of the kittens array:
+//Arrays destructivelyPrependKitten(name) prepends a kitten to the beginning of the kittens array:
 function destructivelyPrependKitten(name){
-  //var name = "Ralph"
   kittens.unshift(name);
   return kittens
 }
 
-//3) Arrays destructivelyRemoveFirstKitten() removes the First kitten from the kittens array:
+//Arrays destructivelyRemoveFirstKitten() removes the First kitten from the kittens array:
 function destructivelyRemoveFirstKitten(){
   kittens.shift(1);
   return kittens
 }
 
-//1) Arrays destructivelyRemoveLastKitten() removes the last kitten from the kittens array:
+//Arrays destructivelyRemoveLastKitten() removes the last kitten from the kittens array:
 function destructivelyRemoveLastKitten(){
   kittens.pop(-1);
 }
+
